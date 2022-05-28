2f58e74e631b6b01facd1146e7ec84887f73d97f
gararanjeet <ranjeetgara123@gmail.com>
2022-05-28 22:30:38 +0530

    modify nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { useEffect, useState } from "react/cjs/react.development";
+import React, { useEffect, useState } from "react";
+// import { useEffect, useState } from "react/cjs/react.development";
 import "./nav.css";
 
 function Nav() {
